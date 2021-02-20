0a566fd0a0bc405d899df18ab763421d285a344c
joepalmo <47197782+joepalmo@users.noreply.github.com>
2021-02-20 17:43:46 -0500

    Add files via upload

--- accretion.py
+++ accretion.py
@@ -26,6 +26,7 @@ import pdb
 import glob
 import scipy.optimize as optimization
 import scipy.interpolate as sinterp
+from sklearn.neighbors import KernelDensity
 
 ################### Mdot Calculations #########################
 
@@ -36,7 +37,7 @@ def flux_to_luminosity(flux, distance):
     '''
     This function will turn a flux estimate (erg/s/cm^2) into a luminosity using distance
     '''
-    lum = flux * (4*np.pi*(dist*const.pc.to('cm').value)**2)
+    lum = flux * (4*np.pi*(distance*const.pc.to('cm').value)**2)
     return lum
     
 
@@ -91,7 +92,7 @@ def Mdot_to_Lacc(Mdot, mass, radius, Rin=5):
     return Lacc
     
     
-def UVExcess_to_Mdot(UVexcess, bc, dist, mass, radius, Av, Rin=5, ):
+def UVExcess_to_Mdot(UVexcess, bc, dist, mass, radius, Av, Rin=5):
     '''
     This function will transform a UV Excess flux value into a mass accretion rate estimate by following 
     the process described in Herczeg 2008. 
@@ -113,7 +114,7 @@ def UVExcess_to_Mdot(UVexcess, bc, dist, mass, radius, Av, Rin=5, ):
     deredUV = ex.remove(Av, UVexcess)
     
     #use the bolometric correction factor to scale the UV excess flux to total accretion flux
-    total_excess = UderedUV*bc
+    total_excess = deredUV*bc
     
     #accretion flux to accretion luminosity
     Lacc = flux_to_luminosity(total_excess, dist)
@@ -127,7 +128,7 @@ def UVExcess_to_Mdot(UVexcess, bc, dist, mass, radius, Av, Rin=5, ):
     return Mdot
     
     
-def Mdot_to_UVExcess(Mdot, bc, dist, mass, radius, Rin=5):
+def Mdot_to_UVExcess(Mdot, bc, dist, mass, radius, Av, Rin=5):
     '''
     This function will transform a mass accretion rate estimate to a UV Excess flux value by following 
     the process described in Herczeg 2008. 
@@ -162,7 +163,7 @@ def Mdot_to_UVExcess(Mdot, bc, dist, mass, radius, Rin=5):
     
     return redUV
     
-    
+
 def UbandExcess_to_Mdot(Uexcess, dist, mass, radius, Av, Rin=5, unc=False):
     '''
     This function will transform a U band flux value into a mass accretion rate estimate by following 
@@ -204,7 +205,7 @@ def UbandExcess_to_Mdot(Uexcess, dist, mass, radius, Av, Rin=5, unc=False):
     
 
 
-def lineflux_to_Mdot(flux, dist, mass, radius, Rin=5, line=None, A=None, B=None):
+def lineflux_to_Mdot(flux, dist, mass, radius, Av, Rin=5, line=None, A=None, B=None):
     '''
     This function will turn a line flux into a mass accretion rate estimate using the Lacc-Lline fits derived
     by Alcala et al 2017. 
@@ -218,8 +219,8 @@ def lineflux_to_Mdot(flux, dist, mass, radius, Rin=5, line=None, A=None, B=None)
     Optional:
     Rin (float) : magnetospheric radius (default 5 [Rsun])
     line (str) : type of line. for now acceptable inputs are H-alpha: 'Ha', Pa-beta: 'Pab', and Br-gamma: 'Brg'
-    a (float) : If you want to input the parameters for your own line flux vs Lacc relationship
-    b (float) : If you want to input the parameters for your own line flux vs Lacc relationship
+    A (float) : If you want to input the parameters for your own line flux vs Lacc relationship
+    B (float) : If you want to input the parameters for your own line flux vs Lacc relationship
     
     Outputs:
     Mdot (float) : mass accretion rate [Msun/yr]
@@ -243,8 +244,12 @@ def lineflux_to_Mdot(flux, dist, mass, radius, Rin=5, line=None, A=None, B=None)
         print('Line not found.')
         return
     
+    #extinction correction
+    deredflux = ex.remove(Av, flux)
+    
     #find Lline in erg/s
-    Lline = flux * (4*np.pi*(dist*const.pc.to('cm').value)**2)
+    Lline = deredflux * (4*np.pi*(dist*const.pc.to('cm').value)**2)
+
     #convert to solar luminosity
     Lline = Lline / const.L_sun.to('erg/s').value
     
@@ -253,9 +258,77 @@ def lineflux_to_Mdot(flux, dist, mass, radius, Rin=5, line=None, A=None, B=None)
     #solar luminosity
     Lacc = unlog(logLacc)
     
-    Mdot = Lacc_to_Mdot(Lacc, mass, radius)
+    Mdot = Lacc_to_Mdot(Lacc, mass, radius, Rin=Rin)
     
     return Mdot
+    
+    
+    
+def Mdot_to_lineflux(Mdot, dist, mass, radius, Av, Rin=5, line=None, A=None, B=None):
+    '''
+    This function will turn a mass accretion rate estimate into a line flux using the Lacc-Lline fits derived
+    by Alcala et al 2017. 
+    
+    Inputs:
+    Mdot (float) : mass accretion rate [Msun/yr]
+    dist (float) : distance to object [pc]
+    mass (float) : mass of object [Msun]
+    radius (float) : radius of object [Rsun]
+    
+    Optional:
+    Rin (float) : magnetospheric radius (default 5 [Rsun])
+    line (str) : type of line. for now acceptable inputs are H-alpha: 'Ha', Pa-beta: 'Pab', and Br-gamma: 'Brg'
+    A (float) : If you want to input the parameters for your own line flux vs Lacc relationship
+    B (float) : If you want to input the parameters for your own line flux vs Lacc relationship
+    
+    Outputs:
+    flux (float) : line flux [erg/(s*cm^2)]?
+    '''
+    
+    #a & b values pulled directly from the paper
+    
+    if line == None:
+        a = A
+        b = B
+    elif line == 'Ha':
+        a=1.13 #+/- 0.05
+        b=1.74  #+/- 0.19
+    elif line == 'Pab':
+        a=1.06 #+/- 0.07
+        b=2.76  #+/- 0.34
+    elif line == 'Brg':
+        a=1.19 #+/- 0.10
+        b=4.02  #+/- 0.51
+    else:
+        print('Line not found.')
+        return
+    
+    # Mdot to Lacc
+    Lacc = Mdot_to_Lacc(Mdot, mass, radius, Rin=Rin)
+    
+    #log(Lacc)
+    logLacc = np.log10(Lacc)
+    
+    #Find log(Lline) (in solar luminosity) using Alcala relationships
+    logLline = (logLacc-b)/a
+    
+    #unlog, convert Lline to erg/s
+    Lline = unlog(logLline)
+    Lline = Lline * const.L_sun.to('erg/s').value
+    
+    #convert Lline to flux
+    flux = Lline / ((4*np.pi*(dist*const.pc.to('cm').value)**2))
+    
+    #extinction correction
+    redflux = ex.apply(Av, flux)
+    
+    return redflux
+    
+
+def empiricalMdot(mass, scalefactor=1.87, intercept=7.73):
+    Mdot = (mass**scalefactor) / (10**intercept)
+    
+    return (Mdot)
 
 
 ################### Object Parameter Estimation #########################
@@ -266,6 +339,28 @@ models = glob.glob('StellarParams/Baraffe*txt')
 mesamodels = glob.glob('StellarParams/MESA_*.txt')
 
 
+def Num_to_SpTy(spectypenum):
+    '''
+    This function takes a spectral type in the form - 'letternumber', i.e. 'M5'.
+    It then translates that spec type into a spectral type number identifier used for interpolation.
+    '''
+    spty_dict = {0 : 'B',
+                 1 : 'A',
+                 2 : 'F',
+                 3 : 'G',
+                 4 : 'K',
+                 5 : 'M'}
+    
+    if int(spectypenum/10) > 5:
+        letter = 'M'
+        number = int(spectypenum%10)+10
+    else:
+        letter = spty_dict[int(spectypenum/10)]
+        number = int(spectypenum%10)
+    
+    spty = letter+str(number)
+    
+    return spty
 
 def to_SpTyNum(spectype):
     '''
@@ -330,11 +425,16 @@ def Teff_to_params(Teff, age):
             
     mass, teff, loglum, logg, radius = np.loadtxt(model, skiprows=1).T
     
-    p_mass = np.polyfit(teff, mass, 10)
-    f_mass = np.poly1d(p_mass)
+    #Polynomial Fit -- doesn't work outside of model grid boundaries
+    #p_mass = np.polyfit(teff, mass, 10)
+    #f_mass = np.poly1d(p_mass)
+    f_mass = sinterp.interp1d(teff, mass, fill_value='extrapolate', kind='linear')
     
-    p_radius = np.polyfit(teff, radius, 10)
-    f_radius = np.poly1d(p_radius)
+    #Polynomial Fit -- doesn't work outside of model grid boundaries
+    #p_radius = np.polyfit(teff, radius, 10)
+    #fradius = np.poly1d(p_radius)
+    #newteff = np.linspace(min(teff), max(teff), 10000)
+    f_radius = sinterp.interp1d(teff, radius, fill_value='extrapolate', kind='linear')
     
     m = f_mass(Teff)
     r = f_radius(Teff)
@@ -361,8 +461,10 @@ def mass_to_Teff(massobj, age):
             
     mass, teff, loglum, logg, radius = np.loadtxt(model, skiprows=1).T
     
-    p_teff = np.polyfit(mass, teff, 10)
-    f_teff = np.poly1d(p_teff)
+    #Polynomial Fit -- doesn't work outside of model grid boundaries
+    #p_teff = np.polyfit(mass, teff, 10)
+    #f_teff = np.poly1d(p_teff)
+    f_teff = sinterp.interp1d(mass, teff, fill_value='extrapolate', kind='linear')
     
     t = f_teff(massobj)
     
@@ -385,6 +487,56 @@ def Teff_to_SpTy(teff):
     
     return spty
     
+
+'''
+########### 2D Interpolation for Object Parameters ############
+###### code shown below, functions saved as pickle files ######
+
+from astropy.io import ascii, fits
+#Load in the isochrones
+iso = ascii.read('BHAC15.dat')
+#Interpolate between the models. This may take a minute.
+    
+print('Creating interpolation function for T')
+MtoTfunc = sinterp.interp2d(iso['M'], iso['logT'], iso['Teff'], kind = 'linear')
+    
+print('Creating interpolation function for R')
+MtoRfunc = sinterp.interp2d(iso['M'], iso['logT'], iso['R'], kind = 'linear')   
+    
+print('Creating interpolation function for M')
+TtoMfunc = sinterp.interp2d(iso['Teff'], iso['logT'], iso['M'], kind = 'linear')
+    
+print('Creating interpolation function for R')
+TtoRfunc = sinterp.interp2d(iso['Teff'], iso['logT'], iso['R'], kind = 'linear')
+
+with open('MtoTfunc.pickle', 'wb') as f:
+    pickle.dump(MtoTfunc, f)
+
+with open('MtoRfunc.pickle', 'wb') as f:
+    pickle.dump(MtoRfunc, f)
+
+with open('TtoMfunc.pickle', 'wb') as f:
+    pickle.dump(TtoMfunc, f)
+    
+with open('TtoRfunc.pickle', 'wb') as f:
+    pickle.dump(TtoRfunc, f)
+    
+######## load in MtoTfunc.pickle, MtoRfunc.pickle, TtoMfunc.pickle, TtoRfunc.pickle to use #########
+    
+with open('MtoTfunc.pickle', 'rb') as f:
+    T = pickle.load(f)
+    
+with open('MtoRfunc.pickle', 'rb') as f:
+    R = pickle.load(f)
+    
+with open('TtoMfunc.pickle', 'rb') as f:
+    TtoMfunc = pickle.load(f)
+    
+with open('TtoRfunc.pickle', 'rb') as f:
+    TtoRfunc = pickle.load(f)
+'''    
+    
+
 ################### Uncertainty Distributions #########################
 
 
@@ -396,7 +548,7 @@ def cdf(g, dx):
     cdf = np.cumsum(g) * dx
     return cdf
     
-    
+
 def inverse_transform_sampling(x, f, nsamples):
     '''
     This function performs the inverse transform sampling method to sample from a probability distribution 
@@ -412,11 +564,96 @@ def inverse_transform_sampling(x, f, nsamples):
     samples (array-like) - an inverse transform sampled distribution which resembles the PDF
     '''
     
-    normalized = f / np.trapz(f, x=x)
     dx = x[1]-x[0]
+    normalized = f / np.trapz(f, x=x, dx=dx)
     c = cdf(normalized, dx)
-    inv = sinterp.interp1d(c, x)
+    inv = sinterp.interp1d(c, x, fill_value='extrapolate')
     r = np.random.rand(nsamples)
     samples = inv(r)
+    samples = samples[np.isfinite(samples)]
     
     return np.array(samples)
+    
+def uncdist(x, mu, sigma, nsamples):
+    '''
+    Builds a truncated gaussian uncertainty distribution for a parameter using inverse transform sampling.
+    
+    Inputs:
+    x (array-like) - the x-values used to generate f
+    mu (float) - mean value of the parameter
+    sigma (float) - uncertainty of the parameter
+    nsamples (int) - the number of inverse transform sampled values needed, i.e. the size of the output
+    
+    Outputs:
+    samples (array-like) - an inverse transform sampled distribution which resembles the PDF
+    '''
+    
+    f = gaussian(x, mu, sigma)
+    samples = inverse_transform_sampling(x, f, nsamples)
+    if nsamples == 1:
+        if samples.size != 0:
+            return samples[0]
+        else:
+            return np.nan
+    else:
+        return samples
+
+
+def pdf_fit(x, data, kernel='gaussian'):
+    '''
+    This function fits a probability density function to input data (data) using a non-parametric
+    kernel density estimation.
+    
+    Inputs:
+    x (array-like) - range of x-values used to generate the pdf, must cover the range of the data
+    data (array-like) - must be in order from least to greatest, and contain no NaN values
+    
+    Optional:
+    kernel (str) - the kernel to use
+                {‘gaussian’, ‘tophat’, ‘epanechnikov’, ‘exponential’, ‘linear’, ‘cosine’}, default=’gaussian’
+    
+    Outputs:
+    pdf (array-like) - a fitted probability density function for the histogram of data
+    '''
+    
+    model = KernelDensity(bandwidth=2, kernel=kernel)
+    d = data.reshape((len(data), 1))
+    model.fit(d)
+    
+    pdf = model.score_samples(x.reshape((len(x), 1)))
+    pdf = np.exp(pdf)
+    
+    return pdf
+
+
+    
+    
+    
+################### IMFs #########################
+
+def MillerScalo1979(m, size):
+    x = np.copy(m)
+    a1 = 0   # m < 1
+    a2 = 2.3  # m > 1
+    m[m<1] = (m[m<1]**(-a1))
+    m[(m>1)] = (m[(m>1)]**(-a2))
+    inv = inverse_transform_sampling(x, m, size)
+    return inv
+    
+def Romano2005(m, size):
+    x = np.copy(m)
+    
+    # Chabrier IMF info from Romano+ 2005
+    mc = 0.079 #Msun
+    sigma = 0.69
+    fa = 0.85#1.06
+    fb = 0.24#0.30
+    c = 1.3#1.7
+    m[m<=1] = fa * np.exp(-1 * ((np.log10(m[m<=1]) - np.log10(mc))**2. / (2*sigma**2))) 
+    m[m>1] = fb * m[m>1]**(-c)
+    inv = inverse_transform_sampling(x, m, size)
+    return inv
+    
+    
+    
+    
